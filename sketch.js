915ee2103419159e9401ad4b6bfe2d40d3d1209d
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-05-14 15:24:30 +0200

    FEAT: all bals rollinggit add --all  FIXED - Player can Play

--- sketch.js
+++ sketch.js
@@ -1,68 +1,114 @@
-document.getElementById('motion').onclick = requestMotionPermission;
-
 let deltaX, deltaY;
-const w =window.innerWidth;
+const w = window.innerWidth;
 const h = window.innerHeight;
-const beginX = w /2;
-const beginY = h/2;
+const beginX = w / 2;
+const beginY = h / 2;
+
+let player;
+let circles = [];
+let score = 0;
+
 function setup() {
   createCanvas(w, h);
+  angleMode(DEGREES);
+
+  player = new Ball(beginX, beginY, 70, "#A020F0");
+
+  for (let i = 0; i < 10; i++) {
+    let circle = new Circle(random(width), random(height), 20, "#0000ff");
+    circles.push(circle);
+  }
 }
 
 function draw() {
-  background(0, 225, 225);
-  fill(255);
-  strokeWeight(1.5);
-  fill(255);
-  circle(beginX-50, beginY, ball.size + 10);
-  
-  ball.show();
-  angleMode(DEGREES);
-  deltaX =  map(rotationX, -30, 30, -10, 10);
+  background(0, 225, 0);
+
+  player.show();
+
+  deltaX = map(rotationX, -30, 30, -10, 10);
   deltaY = map(rotationY, -30, 30, -10, 10);
-  ball.move(deltaX,deltaY);
+  player.move(deltaX, deltaY);
+
+  for (let i = circles.length - 1; i >= 0; i--) {
+    circles[i].show();
+    circles[i].move();
+
+    if (player.hits(circles[i])) {
+      circles.splice(i, 1);
+      score++;
+    }
+  }
+  
+
   fill(0);
   textAlign(CENTER);
+  textSize(24);
+  text("Score: " + score, w / 2, 40);
 
- // text("(" + rotationX + " , "+ rotationY + ")", w/2,15);
+  if (circles.length == 0) {
+    text("You win!", width / 2, height / 2);
+    noLoop();
+  }
 }
 
-class Ball{
-  constructor(x,y,s,c){
+class Ball {
+  constructor(x, y, s, c) {
     this.x = x;
     this.y = y;
     this.size = s;
-    this.color= c;
+    this.color = c;
   }
-  
-  show(){
+
+  show() {
     push();
     noStroke();
     fill(this.color);
-    circle(this.x,this.y,this.size);
+    circle(this.x, this.y, this.size);
     pop();
   }
 
-  move(xOff,yOff){
-    
+  move(xOff, yOff) {
     this.x += xOff;
     this.y -= yOff;
-    let r = this.size/2;
-    if(this.x - r < 0){
-        this.x = 0 + r;
+    let r = this.size / 2;
+    if (this.x - r < 0) {
+      this.x = 0 + r;
     }
-    if(this.x + r > width ){
+    if (this.x + r > width) {
       this.x = width - r;
     }
-    if(this.y - r < 0){
-      this.y = 0  +r;
+    if (this.y - r < 0) {
+      this.y = 0 + r;
     }
-    if(this.y + r > height ){
+    if (this.y + r > height) {
       this.y = height - r;
     }
-    
-    
+  }
+
+  hits(circle) {
+    let d = dist(this.x, this.y, circle.x, circle.y);
+    return d < (this.size / 2) + (circle.size / 2);
   }
 }
 
-const ball = new Ball(beginX,beginY,70,"#ff8e0d");
+class Circle {
+  constructor(x, y, s, c) {
+    this.x = x;
+    this.y = y;
+    this.size = s;
+    this.color = c;
+  }
+
+  show() {
+    push();
+    noStroke();
+    fill(this.color);
+    circle(this.x, this.y, this.size);
+    pop();
+  }
+
+  move() {
+    this.x += random(-2, 2);
+    this.y += random(-2, 2);
+  }
+}
